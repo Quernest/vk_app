264ade76370eafb2d25db7ccae68325db1e4bb5e
Пользователь <a.zhit4enko@gmail.com>
2017-05-14 08:02:21 +0300

    change breakpoint

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,10 +6,19 @@ const USERS = 'users';
 const STATUS = 'status';
 const NEWSFEED = 'newsfeed';
 
+// breakpoints
+const PHONE = 543;
+const TABLET_PORTRAIT_UP = 768;
+const TABLET_LANDSCAPE_UP = 992;
+
 export {
   POSTER,
   FRIENDS,
   USERS,
   STATUS,
-  NEWSFEED
+  NEWSFEED,
+  PHONE,
+  TABLET_PORTRAIT_UP,
+  TABLET_LANDSCAPE_UP
 };
+
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { TABLET_LANDSCAPE_UP } from '../constants/constants';
 
 import Header    from '../components/Header';
 import Sidebar   from '../components/Sidebar/Sidebar';
@@ -11,7 +12,7 @@ export default class Home extends Component {
     const { data, onClick, data: { windowWidth } } = this.props;
 
     return (
-      <div id='wrapper' className={classNames({ 'toggled': windowWidth < 768 })}>
+      <div id='wrapper' className={classNames({ 'toggled': windowWidth < TABLET_LANDSCAPE_UP })}>
         <Header onClick={onClick} />
         <Sidebar data={data} onClick={onClick} />
         <Dashboard data={data} onClick={onClick} />
